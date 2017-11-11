b63a3acaf8bb09369699b16adc8528182e28af55
Adam Fudge <coolfudge@gmail.com>
2017-11-11 01:50:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,22 @@
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  def initialize
+  end
+
+  def create_table
+  end
+
+  def drop_table
+  end
+
+  def save
+  end
+
+  def create
+  end
   
 end
