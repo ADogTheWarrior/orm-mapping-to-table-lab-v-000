809429a518358a3d89cadda950b518e8e7bd42d0
Adam Fudge <coolfudge@gmail.com>
2017-11-11 02:13:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,10 +3,9 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade, id=nil)
-    @name = name
-    @grade = grade
-    @id = id
+  def initialize(attributes)
+    @id = nil
+    attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.create_table
